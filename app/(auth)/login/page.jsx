4faa2c84012ccdd6bc942c8e7b16b7f83a0b0b8f
Pani <ivan131335@gmail.com>
2026-05-14 14:38:02 +0700

    feat: make callback route for login

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/`,
+        redirectTo: `${window.location.origin}/auth/callback`, // ✅ ke callback dulu
       },
     });
     if (error) {
