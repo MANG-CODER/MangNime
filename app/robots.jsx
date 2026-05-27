46516494455b7e3769fb75be556b0afaf748ac25
Pani <ivan131335@gmail.com>
2026-05-27 19:40:13 +0700

    feat: robots.jsx new rules

--- app/robots.jsx
+++ app/robots.jsx
@@ -1,11 +1,16 @@
 export default function robots() {
   return {
-    rules: {
+    rules: [
+    {
+        userAgent: ['ClaudeBot', 'GPTBot', 'ChatGPT-User', 'CCBot'],
+        disallow: '/',
+      },
+      {
       userAgent: "*",
       allow: "/",
       disallow: "/komik/*/*", // Memblokir semua rute chapter
     },
     // Opsional: Kalau nanti Anda punya sitemap
     // sitemap: 'https://mangnime.vercel.app/sitemap.xml',
-  };
+  ]};
 }
