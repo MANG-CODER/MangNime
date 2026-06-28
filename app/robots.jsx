8c20c84571bd91f6693625e6892674b1d86aa6db
Pani <ivan131335@gmail.com>
2026-06-28 13:20:14 +0700

    feat: robots & vercel update

--- app/robots.jsx
+++ app/robots.jsx
@@ -2,17 +2,20 @@ export default function robots() {
   return {
     rules: [
       {
-        userAgent: ["ClaudeBot", "GPTBot", "ChatGPT-User", "CCBot"],
+        userAgent: [
+          "ClaudeBot",
+          "GPTBot",
+          "ChatGPT-User",
+          "CCBot",
+          "meta-externalagent",
+          "facebookexternalhit",
+        ],
         disallow: "/",
       },
       {
         userAgent: "*",
         allow: "/",
-        disallow: [
-          "/komik/*/*",
-          "/api/",
-          "/_next/",
-        ],
+        disallow: ["/komik/*/*", "/api/", "/_next/"],
       },
     ],
     sitemap: "https://mangnime.my.id/sitemap.xml",
